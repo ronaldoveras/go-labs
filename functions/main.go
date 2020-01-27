ad0dcc2262775d5e74fa0db86cde21337fba1fcf
00784094462 <ronaldo.veras@serpro.gov.br>
2020-01-27 13:01:08 -0300

    Usando ponteiros

--- functions/main.go
+++ functions/main.go
@@ -7,10 +7,29 @@ import (
 func main() {
 	d := soma(4, 6)
 	fmt.Println(d)
+	p := 10
+
+	r := double(p)
+	fmt.Printf("Valor original: %d \n", p)
+	fmt.Printf("Valor dobrado: %d \n", r)
+
+	q := doublePointer(&p)
+	fmt.Printf("Valor original (pointer): %d \n", p)
+	fmt.Printf("Valor dobrado (pointer): %d \n", q)
 
 }
 
 func soma(a int32, b int32) (z int32) {
 	z = a + b
 	return
+}
+
+func doublePointer(a *int) int {
+	*a = 2 * (*a)
+	return *a
+}
+
+func double(a int) int {
+	a = 2 * (a)
+	return a
 }
